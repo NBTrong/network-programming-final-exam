17d4629cd29132942c64094cfe6f01648aa6bb82
NBTrong <trong.nb121001@gmail.com>
2024-01-10 17:05:52 +0700

    bugfix: Log file

--- TCP_Server/src/global.c
+++ TCP_Server/src/global.c
@@ -1,7 +1,11 @@
 #include "global.h"
 
 char currentUser[STRING_LENGTH] = "\0";
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int in_game[MAX_CLIENTS];
+
+pthread_mutex_t session_mutex;
+pthread_mutex_t challenge_mutex;
 pthread_mutex_t socket_mutex;
+pthread_mutex_t game_mutex;
+pthread_mutex_t room_mutex;
 pthread_cond_t cond;
